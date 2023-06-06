7b1ee2f407d163f6431b9338bd9f0ff77a46088c
srpsmruti <srp.smruti@gmail.com>
2023-06-06 20:05:09 +0530

    Update DriverInstance.java

--- src/main/java/com/framework/selenium/api/base/DriverInstance.java
+++ src/main/java/com/framework/selenium/api/base/DriverInstance.java
@@ -31,6 +31,7 @@ public class DriverInstance  extends AbstractTestNGCucumberTests{
 			ChromeOptions chrome_options = new ChromeOptions();
 			chrome_options.addArguments("--start-maximized"); 
 			chrome_options.addArguments("--disable-notifications"); 
+			chromeOptions.add_argument("--remote-debugging-port=9222")
 			remoteWebdriver.set(new ChromeDriver(chrome_options));
 			break;
 		case "firefox":
